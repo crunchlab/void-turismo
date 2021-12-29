289772284595909b9d473d8144e096fab429df7a
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2021-12-29 20:18:46 +0100

    VOIDTUR-1: handled resize to fit full viewport

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import * as maplibregl from 'maplibre-gl';
+import { MapboxEvent } from 'maplibre-gl';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    @ViewChild('homeMap') homeMap: maplibregl.Map;
 
   constructor() {}
 
+    public maploaded(event:any){
+        event.resize();
+    }
 }
