4e9f681b3587be09873135c14cc1e1245fb1a1b1
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2021-12-30 23:13:27 +0100

    VOIDTUR-2: added pop up stub

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,38 @@
-import { Component, ViewChild } from '@angular/core';
-import { FeatureCollection } from 'geojson';
+import { Component } from '@angular/core';
 import * as maplibregl from 'maplibre-gl';
-import { MapboxEvent } from 'maplibre-gl';
+import { get as _get } from 'lodash';
 import { FeatureCollectionService } from '../api/feature-collection.service';
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-    @ViewChild('homeMap') homeMap: maplibregl.Map;
+    homeMap: maplibregl.Map;
+    selectedFeature: any = { lngLat: [0, 0] };
+    public get = _get;
     // featureCollection: any;
 
-  constructor(featureCollectionService:FeatureCollectionService) {
-    //   featureCollectionService.list().subscribe((featureCollection: FeatureCollection)=>{
-    //       this.featureCollection = { 'type': 'geojson', data:featureCollection};
-    //   });
-  }
+    constructor(featureCollectionService: FeatureCollectionService) {
+        //   featureCollectionService.list().subscribe((featureCollection: FeatureCollection)=>{
+        //       this.featureCollection = { 'type': 'geojson', data:featureCollection};
+        //   });
+    }
 
-    public maploaded(event:any){
+    public maploaded(event: any) {
+        this.homeMap = event;
+        this.homeMap.on('click', 'strutture-layer', (e: any) => {
+            // Copy coordinates array.
+            const coordinates = this.get(e, 'features[0].geometry.coordinates', []).slice();
+            const description = this.get(e, 'features[0].properties.DENOMINAZIONE');
+
+            new maplibregl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(description)
+                .addTo(this.homeMap);
+        });
         event.resize();
     }
+
+
 }
