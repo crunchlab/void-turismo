1a62c2b660e81b86673868aa80b185a7325bb269
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-01-08 19:09:47 +0100

    VOIDTUR-7: added searchbar with autocomplete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import * as maplibregl from 'maplibre-gl';
-import { get as _get, isNil } from 'lodash';
+import { get as _get, isNil, uniq } from 'lodash';
 import { Struttura } from '../models/struttura/struttura';
 import { FeatureToStrutturaService } from '../services/transformer/feature-to-struttura.service';
 import { Feature, Geometry } from 'geojson';
 import SwiperCore, { Pagination, Virtual } from 'swiper';
-import { TranslocoService } from "@ngneat/transloco";
 
 SwiperCore.use([Pagination, Virtual]);
 @Component({
@@ -38,7 +37,8 @@ export class HomePage {
         };
     featureTransformer: FeatureToStrutturaService;
     strutture: Struttura[] = [];
-    // featureCollection: any;
+    comuni: string[] = [];
+    comuniCandidati: string[] = [];
 
     constructor(featureTransformer: FeatureToStrutturaService) {
         this.featureTransformer = featureTransformer;
@@ -46,6 +46,7 @@ export class HomePage {
 
     public mapLoaded(event: any) {
         this.homeMap = event;
+        this.homeMap.on('sourcedata', e=>this.sectionSourceAddedCallback(e));
         this.homeMap.on('click', 'strutture-layer', (e: any) => {
             let clickedFeature = this.get(e, 'features[0]', null);
             if (!isNil(clickedFeature)) {
@@ -58,16 +59,26 @@ export class HomePage {
                 this.handleLayerClick(clickedFeature);
             }
         });
-        this.strutture = this.homeMap.queryRenderedFeatures(null, { "layers": ["strutture-layer"] }).map((feature: Feature) => this.featureTransformer.featureToStruttura(feature));
+
 
         event.resize();
     }
+    sectionSourceAddedCallback(e: maplibregl.EventData): void {
+        if (this.homeMap.getSource('strutture') &&
+            this.homeMap.isSourceLoaded('strutture') &&
+            e.isSourceLoaded) {
+            this.strutture = this.homeMap.queryRenderedFeatures(null, { "layers": ["strutture-layer"] }).map((feature: Feature) => this.featureTransformer.featureToStruttura(feature));
+            this.comuni = uniq(this.strutture.map((s: Struttura) => s.comune));
+
+            this.homeMap.off('sourcedata', this.sectionSourceAddedCallback); //Unbind event here
+        }
+    }
+
     public mapZoomEnd(event: any) {
         this.strutture = this.homeMap.queryRenderedFeatures(null, { "layers": ["strutture-layer"] }).map((feature: Feature) => this.featureTransformer.featureToStruttura(feature));
     }
     private handleLayerClick(clickedFeature: Feature<Geometry, { [name: string]: any; }>) {
         let struttura: Struttura = this.featureTransformer.featureToStruttura(clickedFeature);
-        console.log(struttura);
         const coordinates = this.get(clickedFeature, 'geometry.coordinates', []).slice();
         const description = this.get(clickedFeature, 'properties.denominazione');
         const htmlContent = `<h4 class="tooltip_title">${description}</h4>`;
@@ -77,5 +88,13 @@ export class HomePage {
             .addTo(this.homeMap);
     }
 
+    public onSearchItemChange(event: any) {
+        let searchTerm: string = _get(event, 'detail.value', '');
+        if (searchTerm) {
+            this.comuniCandidati = this.comuni.filter(c => (c.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1));
+        } else {
+            this.comuniCandidati = [];
+        }
+    }
 
 }
