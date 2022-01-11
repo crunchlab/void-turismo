e255d2b5b1d18ce2eb2bce459869e57a10a8778b
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-01-11 13:29:53 +0100

    VOIDTUR-5: fix json typing and add cast

--- src/app/services/api/struttura.service.ts
+++ src/app/services/api/struttura.service.ts
@@ -1,28 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Struttura } from 'src/app/models/struttura/struttura';
-import { Feature, Geometry } from 'geojson';
-
 import { find } from 'lodash';
 import { FeatureToStrutturaService } from '../transformer/feature-to-struttura.service';
-import { HttpClient } from '@angular/common/http';
+
+import strutture from '../../../assets/data/strutture.json';
 @Injectable({
     providedIn: 'root'
 })
 export class StrutturaService {
     strutture: Struttura[] = [];
 
-    constructor(private service: FeatureToStrutturaService, private httpClient: HttpClient) { }
-
-
-    ngOnInit() {
-        this.httpClient.get<any>("assets/data/strutture.json").subscribe((data) =>
-            this.strutture = data.map(d => this.service.featureToStruttura(d))
-        );
+    constructor(private transformer: FeatureToStrutturaService) {
+        this.strutture = strutture.features.map((f: any) => this.transformer.featureToStruttura(f));
     }
 
 
     public getDetail(id: string | number): Struttura {
-        let struttura: Struttura = find(this.strutture, { codiceIdentificativo: id }) as Struttura;
+        let struttura: Struttura = find(this.strutture, (s: Struttura) => s.codiceIdentificativo == (id as number)) as Struttura;
         return struttura;
     }
 }
