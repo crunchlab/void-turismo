8135074e0df41eb3b79fa29f89ce3aa793ba68d3
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-01-20 21:39:13 +0100

    VOIDTUR-9: hidden non matching circles

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -43,8 +43,6 @@ export class HomePage implements OnInit {
         },
         'circle-color': [
             'case',
-            // ['!', ['boolean', ['feature-state', 'isHighlighted'], true]],
-            // 'transparent',
             ['all', ['boolean', ['feature-state', 'isMatch'], true], ['!', ['boolean', ['feature-state', 'isWithinRange'], true]]],
             COLOR_MAP.tipologia.ALTRA_RICETTIVITA,
             ['all', ['boolean', ['feature-state', 'isMatch'], true], ['==', ['get', 'tipologia'], "ALBERGO"]],
@@ -65,33 +63,8 @@ export class HomePage implements OnInit {
             COLOR_MAP.tipologia.RESIDENCE,
             'transparent'
         ],
-        'circle-stroke-color': [
-            'case',
-            ['any',
-                // ['!', ['boolean', ['feature-state', 'isMatch'], true]],
-                ['!', ['boolean', ['feature-state', 'isWithinRange'], true]]
-            ],
-            COLOR_MAP.tipologia.ALTRA_RICETTIVITA,
-            ['==', ['get', 'tipologia'], "ALBERGO"],
-            COLOR_MAP.tipologia.ALBERGO,
-            ['==', ['get', 'tipologia'], "APPARTAMENTO"],
-            COLOR_MAP.tipologia.AGRITURISMO,
-            ['==', ['get', 'tipologia'], "AGRITURISMO"],
-            COLOR_MAP.tipologia.APPARTAMENTO,
-            ['==', ['get', 'tipologia'], "BED_AND_BREAKFAST"],
-            COLOR_MAP.tipologia.BED_AND_BREAKFAST,
-            ['==', ['get', 'tipologia'], "CAMPEGGIO"],
-            COLOR_MAP.tipologia.CAMPEGGIO,
-            'transparent'
-        ],
+        'circle-stroke-color': 'transparent',
         'circle-stroke-width': 1,
-
-        'circle-stroke-opacity': [
-            'case',
-            ['!', ['boolean', ['feature-state', 'isHighlighted'], true]],
-            0.5,
-            1
-        ],
         'circle-opacity': [
             'case',
             ['!', ['boolean', ['feature-state', 'isHighlighted'], true]],
@@ -112,17 +85,26 @@ export class HomePage implements OnInit {
                 0,
                 1.25
             ],
-            "text-anchor": "top"
+            "text-anchor": "top",
+            "text-size": [
+                'interpolate', ['linear'], ['zoom'],
+                6, 1,
+                7, 2,
+                8, 4,
+                9, 8,
+                10, 14,
+                11, 16,
+                30, 24
+            ]
         };
     public labelPaint: maplibregl.SymbolPaint = {
 
-        "text-opacity": {
-            "stops": [
-                [{ "zoom": 6, "value": 0 }, 0],
-                [{ "zoom": 10, "value": 1 }, 1]
-            ]
-
-        }
+        "text-opacity": [
+            'case',
+            ['boolean', ['feature-state', 'isMatch'], true],
+            1,
+            0
+        ]
     };
 
     strutture: Struttura[] = [];
@@ -138,7 +120,6 @@ export class HomePage implements OnInit {
         this.comuni = uniq(strutture.map((s: Struttura) => s.comune)).sort();
         this.tipologie = uniq(strutture.map((s: Struttura) => s.tipologia)).sort();
         this.tipologieSelezionate = [...this.tipologie];
-
     }
     constructor(private featureTransformer: FeatureToStrutturaService, private filterService: FilterServiceProvider, private mapUtils: MapUtilsService) {
 
