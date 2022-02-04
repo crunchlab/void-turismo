cd836fa9f0ef2890477468acb70e72df18794103
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-02-04 16:44:57 +0100

    VOIDTUR-6: added full mapping to spec

--- src/app/services/api/struttura.service.spec.ts
+++ src/app/services/api/struttura.service.spec.ts
@@ -20,27 +20,13 @@ describe('StrutturaService', () => {
 
   it('Should create an Attribute Filter array with desired properties distinct values', () => {
       let filters:AttributeFilter[]=[];
-      let mappings:FieldMapping[]=[
-          {
-              properties:"tipologia",
-              field:"tipologia",
-              type:"string"
-          },
-          {
-              properties: "nuovaClassificazioneLR11",
-              field: "nuovaClassificazioneLR11",
-              type: "string"
-          },
-          {
-              properties: "trasporti",
-              field: "trasporti",
-              type: "bool"
-          },
-          {
-              properties: "posizione",
-              field: "posizione",
-              type: "bool"
-          }
+      let mappings: FieldMapping[] = [
+          { "field": "tipologia", "properties": "tipologia", "type": "string" },
+          { "field": "nuovaClassificazioneLR11", "properties": "nuovaClassificazioneLR11", "type": "string" },
+          { "field": "posizione", "properties": "posizione", "type": "bool" },
+          { "field": "trasporti", "properties": "trasporti", "type": "bool" },
+          { "field": "accoglienza", "properties": ["accessoDisabili", "giochiBimbi", "animaliAmmessi"], "type": "bool" },
+          { "field": "lingue", "properties": "lingue", "type": "bool" },
       ];
       let expectedValues = ["AFFITTACAMERE", "AGRITURISMO", "ALBERGO", "ALTRA RICETTIVITA'", "APPARTAMENTO", "BED AND BREAKFAST", "CAMPEGGIO", "COUNTRY HOUSE", "RESIDENCE"];
       filters = service.getFilterValues(mappings);
@@ -56,7 +42,7 @@ describe('StrutturaService', () => {
       expect(lr11.property).toBe("nuovaClassificazioneLR11");
       expect(intersection((lr11.value as string[]), lr11expectedValues).length).toBe(lr11expectedValues.length);
 
-      console.log(filters[3])
+      console.log(filters);
   });
   
 });
