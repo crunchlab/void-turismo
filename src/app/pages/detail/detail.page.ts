331a3b4bf90ef28d1b203ba822a4ee7b6c8537ea
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-03-06 15:45:30 +0100

    VOIDTUR-11: set CTA fro contacts

--- src/app/pages/detail/detail.page.ts
+++ src/app/pages/detail/detail.page.ts
@@ -18,8 +18,26 @@ export class DetailPage implements OnInit {
     ngOnInit() {
         this.actRoute.params.subscribe(params => {
             this.struttura = this.strutturaService.getDetail(params.id);
-            console.log(this.struttura);
         });
     }
-
+    goToSite() {
+        let site: string = this.struttura.contatti["www"];
+        let url: URL;
+        try {
+            if (site.match(/http[s]?:/)) {
+                url = new URL(site);
+            } else {
+                url = new URL(`http://${site}`);
+            }
+            window.open(url, '_blank');
+        } catch (e) {
+            alert("indirizzo non valido!")
+        }
+    }
+    writeTo() {
+        window.open(`mailto:${this.struttura.contatti["mail"]}`, '_blank');
+    }
+    phoneTo() {
+        window.open(`tel:${this.struttura.contatti["telefono"]}`, '_se_blanklf');
+    }
 }
