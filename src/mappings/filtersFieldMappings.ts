6e5ccc2852d252a22082af6190df271df3f8ce00
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-03-11 16:04:37 +0100

    VOIDTUR-12: removed transport attribute from filtering

--- src/mappings/filtersFieldMappings.ts
+++ src/mappings/filtersFieldMappings.ts
@@ -4,8 +4,6 @@ const filtersFieldMappings: FieldMapping[] = [
     { "field": "tipologia", "properties": "tipologia", "type": "string" },
     { "field": "nuovaClassificazioneLR11", "properties": "nuovaClassificazioneLR11", "type": "string" },
     { "field": "posizione", "properties": "posizione", "type": "bool" },
-    { "field": "trasporti", "properties": "trasporti", "type": "bool" },
-    // { "field": "accoglienza", "properties": ["accessoDisabili", "giochiBimbi", "animaliAmmessi"], "type": "bool" },
     { "field": "lingue", "properties": "lingue", "type": "bool" },
     {
         "field": "accoglienza",
