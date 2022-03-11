6bcaece4b03bb3a8d83538073e1b4ebb85428f62
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-03-11 18:10:56 +0100

    VOIDTUR-13: stub about page

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.page.html',
-  styleUrls: ['./about.page.scss'],
+    selector: 'app-about',
+    templateUrl: './about.page.html',
+    styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+    constructor(private modalController: ModalController) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    async closeModal() {
+        const close: string = "Modal Removed";
+        await this.modalController.dismiss(close);
+    }
 
 }
